668bd3dd130dba4e54bef0b24cd1ee673c3ec6cd
MaxymBrovarsky <asteixcube@gmail.com>
2018-01-31 11:48:47 +0200

    install devise + form for product

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products	
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new' 
+  post '/products' => 'products#create'
 end
