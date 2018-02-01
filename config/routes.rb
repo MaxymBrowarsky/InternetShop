8622161156ac69f991a1e09854e4c5e1b9e532f0
MaxymBrovarsky <asteixcube@gmail.com>
2018-02-01 18:24:35 +0200

    create menu + kidn of cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products	
+  resources :cart_item
   get '/products' => 'products#index'
   get '/products/new' => 'products#new' 
   post '/products' => 'products#create'
