40510b1483cea12e682e876a98ac13e06b7cd55b
MaxymBrovarsky <asteixcube@gmail.com>
2018-02-26 14:01:37 +0200

    without validation

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products	
-  resources :cart_item
+  resources :cart_items
+  resources :carts
+  resources :categories
+  resources :orders
   get '/products' => 'products#index'
-  get '/products/new' => 'products#new' 
+  get '/products/new' => 'products#new'
+
+  resources :users, only: [:show]
   post '/products' => 'products#create'
+ 
+
 end
